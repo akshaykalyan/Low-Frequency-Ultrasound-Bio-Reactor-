ed9a30ffa43d39ee1a9bb08ee3be31897c0caaa0
Akshay <akshaykalyan2307@gmail.com>
2025-10-27 15:46:57 +0530

    added logos

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ class Dashboard(BoxLayout):
         mb_logo = Image(
             source='logo/mb_light.jpg',  # ← replace with your actual image path
             size_hint_x=None,
-            width=150,
+            width=200,
             allow_stretch=True,
             keep_ratio=True
         )
